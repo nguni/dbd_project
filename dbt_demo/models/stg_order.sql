c7dea63ff6b950a89d236eefd58cf2cc35fdfd3f
nguni <johnmuthokak@gmail.com>
2025-11-21 16:35:38 +0300

    Move dbt project to repository root

--- dbt_demo/models/stg_order.sql
+++
@@ -1,9 +0,0 @@
-WITH raw_orders AS (
-    SELECT
-        id AS order_id,
-        user_id AS customer_id,
-        order_date,
-        status
-    FROM {{ source('jaffle_shop', 'orders') }}
-)
-SELECT * FROM raw_orders
